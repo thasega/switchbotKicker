4687e22c2cf22bdc4d85e3255a635033114a8a5c
Tsuyoshi HASEGAWA <t-haseg@lares.dti.ne.jp>
2025-04-25 17:05:40 +0900

    Fix schedule list color: active=white/sun=red, inactive=blue. Remove checkbox from list view.

--- main.py
+++ main.py
@@ -311,15 +311,31 @@ async def web_server():
                 if D[0]==WKDY:
                     WKDN=D[1]
                     break
-            forms += f'''
+            if n[9]:
+                # アクティブ: サンタイムのみ赤、それ以外は白
+                if HOUR == -2 or HOUR == -3:
+                    timecolor = '#ff4444'
+                else:
+                    timecolor = '#ffffff'
+                forms += f'''
 <form action="/edit" method="post" class="form-row">
 <input type="hidden" name="id" value="{IDNO}">
 <button type="submit" name="action" value="change">{USER.DESC_BUTTON_CHANGE}</button>
 <button type="submit" name="action" value="test">{USER.DESC_BUTTON_EXECTEST}</button>
-<input type="checkbox"{checked} disabled>
-{WKDN} {TIME_STR}　{NAME}
+<span style="color:{timecolor}">{WKDN} {timestr_html}</span>　{NAME}
 </form>
 '''
+            else:
+                # 非アクティブ: 全項目青
+                forms += f'''
+<form action="/edit" method="post" class="form-row">
+<input type="hidden" name="id" value="{IDNO}">
+<button type="submit" name="action" value="change">{USER.DESC_BUTTON_CHANGE}</button>
+<button type="submit" name="action" value="test">{USER.DESC_BUTTON_EXECTEST}</button>
+<span style="color:#4488ff">{WKDN} {timestr_html}　{NAME}</span>
+</form>
+'''
+
 
         if len(DataBase)==0:
             forms += f'<label>{USER.DESC_TEXT_NOSCHEDULE}</label>'
@@ -656,7 +672,7 @@ def getSuntimes(hour, minute):
 wdt = None
 def WDTstart():
     global wdt
-    wdt = machine.WDT(timeout=8000)
+    #wdt = machine.WDT(timeout=8000)
     
 def WDTfeed():
     global wdt
